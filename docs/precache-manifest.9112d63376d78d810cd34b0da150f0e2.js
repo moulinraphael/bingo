71181b84dacd64d7d7ab0283e7e68f37b542278a
Raphael Moulin <vistuo@gmail.com>
2020-03-24 23:32:28 +0100

    With option to redraw

---
+++ docs/precache-manifest.9112d63376d78d810cd34b0da150f0e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8e0ec430a1a1f205e5d9efb3659b8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "448aacc8fe0d57e2f177",
+    "url": "./static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "323dbe715ab8fcb947ca",
+    "url": "./static/js/2.20de5c2c.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "./static/js/2.20de5c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448aacc8fe0d57e2f177",
+    "url": "./static/js/main.b01b6c8b.chunk.js"
+  },
+  {
+    "revision": "365ba66d4da303c1e368",
+    "url": "./static/js/runtime-main.228baef8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.79f67459cbe578b3eb76837251e0cebc.js"
+  "./precache-manifest.9112d63376d78d810cd34b0da150f0e2.js"
 );
 
 self.addEventListener('message', (event) => {
