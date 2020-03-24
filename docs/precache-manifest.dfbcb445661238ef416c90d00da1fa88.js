0a5c9ed0933f3c20c5336244693b8b7aa034471f
Raphael Moulin <vistuo@gmail.com>
2020-03-24 23:13:31 +0100

    Add V1 Bingo Github Pages

---
+++ docs/precache-manifest.dfbcb445661238ef416c90d00da1fa88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3b10480cc71435374ad1c8c86ded8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5908cae1175ccaa7a986",
+    "url": "/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "7e519d5c61e8c8bde610",
+    "url": "/static/js/2.d8424d27.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/static/js/2.d8424d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5908cae1175ccaa7a986",
+    "url": "/static/js/main.d1344fc7.chunk.js"
+  },
+  {
+    "revision": "5018ba84458e34cd06e6",
+    "url": "/static/js/runtime-main.4ec088e6.js"
+  }
+]);
